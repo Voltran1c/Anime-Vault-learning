f01eea2223b5bff6b6e78b24812fe5427bc9a9cc
Watcharapong.K <147409902+Voltran1c@users.noreply.github.com>
2024-08-08 17:25:49 +0700

    Anime Vault

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,12 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { fetchAnime } from "@/app/action";
+
+let page = 2;
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData((prevData) => [...prevData, ...res]);
+        page++;
+      });
+    }
+  }, [inView]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
-            src="./spinner.svg"
+            src="/spinner.svg"
             alt="spinner"
             width={56}
             height={56}
